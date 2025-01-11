32c46969b54df23077b3f2e118d35de425babc92
Sam <17427046+Samillion@users.noreply.github.com>
2025-01-11 15:37:03 +0200

    refactor: don't use margins if osc is visible only temporarily

--- modernz.lua
+++ modernz.lua
@@ -746,7 +746,7 @@ local function update_margins()
     local margins = osc_param.video_margins
 
     -- Don't use margins if it's visible only temporarily.
-    if not state.osc_visible or
+    if not state.osc_visible or get_hidetimeout() >= 0 or
        (state.fullscreen and not user_opts.showfullscreen) or
        (not state.fullscreen and not user_opts.showwindowed)
     then
