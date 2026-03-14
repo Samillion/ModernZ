0bc1fc7874738549093c4528b8bb1eb5074fda69
Keith94 <dominantspace@gmail.com>
2026-03-14 16:49:28 -0600

    refactor: show .0 for whole number playback speeds
    For example, 1× => 1.0× to keep it consistent with fractional speeds.

--- modernz.lua
+++ modernz.lua
@@ -3088,7 +3088,7 @@ local function osc_init()
     --tog_speed
     ne = new_element("tog_speed", "button")
     ne.content = function()
-        return string.format("%g", mp.get_property_number("speed", 1)) .. "×"
+        return (speed % 1 == 0) and string.format("%.1f×", speed) or string.format("%g×", speed)
     end
     ne.visible = (osc_param.playresx >= visible_min_width)
     ne.tooltip_style = osc_styles.tooltip
