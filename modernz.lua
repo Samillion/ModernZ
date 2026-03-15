21c5ec15736599ab3b27bded8e6519954d402d8d
Sam <17427046+Samillion@users.noreply.github.com>
2026-03-15 18:43:45 +0200

    refactor: adjust speed control logic and fix indentation

--- modernz.lua
+++ modernz.lua
@@ -3088,8 +3088,8 @@ local function osc_init()
     --tog_speed
     ne = new_element("tog_speed", "button")
     ne.content = function()
-    local speed = mp.get_property_number("speed", 1)
-        return (speed % 1 == 0) and string.format("%.1f×", speed) or string.format("%g×", speed)
+        local speed = mp.get_property_number("speed", 1)
+        return string.format(speed % 1 == 0 and "%.1f×" or "%g×", speed)
     end
     ne.visible = (osc_param.playresx >= visible_min_width)
     ne.tooltip_style = osc_styles.tooltip
